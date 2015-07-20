8513d6a1426f97ba8844309c070374f78872b61d
//de <code@extremist.digital>
2015-07-19 20:06:07 -0700

    dependence on base more

--- lib/abstractive/refrescar.rb
+++ lib/abstractive/refrescar.rb
@@ -1,84 +1,67 @@
 require 'find'
 require 'rb-inotify'
-require 'celluloid/current'
+require 'abstractive/actor'
 
-module Abstractive
-  class Refrescar
+class Abstractive::Refrescar < Abstractive::Actor
 
-    include Celluloid
+  def initialize(options={})
+    @logger = options[:logger]
+    options[:root] ||= Dir.pwd
+    @root = Array[options[:root]]
+    @debug = options.fetch(:debug, false)
+    @announcing = options.fetch(:announcing, true)
+    @reschedule = options.fetch(:reschedule, false)
+    @after_reload = options.fetch(:after_reload, false)
+    @watcher = INotify::Notifier.new
+    @events = [
+      :close_write,
+      #de :modify         #de This seems to fire twice in certain cases. Used :close_write instead.
+    ]
+    async.reloading
+  end
 
-    def initialize(options={})
-      @logger = options[:logger]
-      options[:root] ||= Dir.pwd
-      @root = Array[options[:root]]
-      @debug = options.fetch(:debug, false)
-      @announcing = options.fetch(:announcing, true)
-      @reschedule = options.fetch(:reschedule, false)
-      @after_reload = options.fetch(:after_reload, false)
-      @watcher = INotify::Notifier.new
-      @events = [
-        :close_write,
-        #de :modify         #de This seems to fire twice in certain cases. Used :close_write instead.
-      ]
-      async.reloading
-    end
+  def reloading
+    set_watchers
+    debug("Started code reloading...") if @debug
+    @watcher.run
+  rescue => ex
+    exception(ex, "Trouble running reloader.")
+    raise
+  end
 
-    def reloading
-      set_watchers
-      debug("Started code reloading...") if @debug
-      @watcher.run
-    rescue => ex
-      exception(ex, "Trouble running reloader.")
-      raise
-    end
-
-    def set_watchers
-      @root.each { |path|
-        Find.find( path ) { |file|
-          if !File.extname(file) == '.rb' and !File.directory? file
-            Find.prune
-          else
-            begin
-              if File.extname(file) == '.rb'
-                debug("Will reload: #{file}") if @debug
-                @watcher.watch(file, *@events) { reload(file) }
-              end
-            rescue => ex
-              exception(ex, "Code Reloading > Trouble setting watchers.")
+  def set_watchers
+    @root.each { |path|
+      Find.find( path ) { |file|
+        if !File.extname(file) == '.rb' and !File.directory? file
+          Find.prune
+        else
+          begin
+            if File.extname(file) == '.rb'
+              debug("Will reload: #{file}") if @debug
+              @watcher.watch(file, *@events) { reload(file) }
             end
+          rescue => ex
+            exception(ex, "Code Reloading > Trouble setting watchers.")
           end
-        }
-      }
-    end
-
-    [:debug, :console, :exception].each { |m|
-      define_method(m) { |*args|
-        begin
-          if @logger and @logger.respond_to?(m)
-            @logger.send(m, *args)
-          else
-            puts "#{m}: ... #{args}"
-          end
-        rescue
-          puts "#{m}: ... #{args}"
         end
       }
     }
-
-    def reload(file)
-      begin
-        load(file)
-        console("Reloaded: #{file}") if @announcing
-      	@watcher.watch(file, *@events) { reload file } if @reschedule
-      	@after_reload.call(file) if @after_reload.is_a? Proc
-      rescue SyntaxError => ex
-        exception(ex, "Code Reloading > Syntax error in #{file}")
-      rescue LoadError => ex
-        exception(ex, "Code Reloading > Missing file: #{file}")
-      end
-    rescue => ex
-      exception(ex, "Trouble reloading file: #{file}")
-    end
-
   end
+
+  def reload(file)
+    begin
+      load(file)
+      console("Reloaded: #{file}") if @announcing
+    	@watcher.watch(file, *@events) { reload file } if @reschedule
+    	@after_reload.call(file) if @after_reload.is_a? Proc
+    rescue SyntaxError => ex
+      exception(ex, "Code Reloading > Syntax error in #{file}")
+    rescue LoadError => ex
+      exception(ex, "Code Reloading > Missing file: #{file}")
+    end
+  rescue => ex
+    exception(ex, "Trouble reloading file: #{file}")
+  end
+
 end
+
